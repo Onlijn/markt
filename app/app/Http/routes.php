f856aa18227ebc4ff1f1210d04c1f62cb9bd295b
Graham <graham@directonlijn.nl>
2016-11-23 21:11:15 +0100

    register page route changed

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -20,15 +20,14 @@ Route::get('users/login', 'Auth\AuthController@getLogin');
 Route::post('users/login', 'Auth\AuthController@postLogin');
 Route::get('users/logout', 'Auth\AuthController@getLogout');
 
-Route::get('users/register', 'Auth\AuthController@getRegister');
-Route::post('users/register', 'Auth\AuthController@postRegister');
 
 /* Authenticated users */
 Route::group(['middleware' => 'auth'], function()
 {
+    Route::get('users/register', 'Auth\AuthController@getRegister');
+    Route::post('users/register', 'Auth\AuthController@postRegister');
     Route::get('users/dashboard', array('as'=>'dashboard', function()
 	{
 	return View('users.dashboard');
 	}));
 });
-	
