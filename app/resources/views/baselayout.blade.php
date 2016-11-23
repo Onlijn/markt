4781ab5064864fcf0226eea295c5b6bef44bd17c
Graham <graham@directonlijn.nl>
2016-11-23 22:12:14 +0100

    test

--- app/resources/views/baselayout.blade.php
+++ app/resources/views/baselayout.blade.php
@@ -11,19 +11,19 @@
 
     </head>
 
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Direct events</a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/dashboard">Direct events</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <li><a href="/dashboard">Home</a></li>
                     <li><a href="/markten">Markten</a></li>
                     @if ($auth)
@@ -37,7 +37,7 @@
     </nav>
 
     <body>
-        <div class="container content">
+        <div class="container-fluid content">
             @yield('content')
         </div><!-- /.container -->
     </body>
