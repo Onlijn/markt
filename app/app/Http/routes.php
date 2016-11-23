4b898d199b12d7eb4cfd57432efff99c72885bd0
Graham <graham@directonlijn.nl>
2016-11-23 16:24:28 +0100

    server test

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* User Authentication */
+Route::get('users/login', 'Auth\AuthController@getLogin');
+Route::post('users/login', 'Auth\AuthController@postLogin');
+Route::get('users/logout', 'Auth\AuthController@getLogout');
+
+Route::get('users/register', 'Auth\AuthController@getRegister');
+Route::post('users/register', 'Auth\AuthController@postRegister');
+
+/* Authenticated users */
+Route::group(['middleware' => 'auth'], function()
+{
+    Route::get('users/dashboard', array('as'=>'dashboard', function()
+	{
+	return View('users.dashboard');
+	}));
+});
+	
