ae252d4e3e7f3a9bfbf00a84e82203a045d12bb2
Graham <graham@directonlijn.nl>
2016-12-05 23:35:16 +0100

    changes

--- app/resources/views/users/markten.blade.php
+++ app/resources/views/users/markten.blade.php
@@ -8,7 +8,7 @@
             <ul class="nav nav-sidebar">
                 <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
                 @foreach ($markten as $markt)
-                    <li><a href="#">$markt->Naam</a></li>
+                    <li><a href="#">{{ $markt->Naam }}</a></li>
                 @endforeach
 
                 <li><a href="#">Analytics</a></li>
