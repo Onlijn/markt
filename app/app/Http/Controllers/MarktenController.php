5324c660163ccde7cc224938d9d7edbc4e558c38
Graham <graham@directonlijn.nl>
2016-12-06 01:09:19 +0100

    changs

--- app/app/Http/Controllers/MarktenController.php
+++ app/app/Http/Controllers/MarktenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Markt as Markt;
 use App\Models\Standhouder as Standhouder;
 use App\Http\Controllers\Controller;
+use App\Http\Reuests\Request;
 
 class MarktenController extends Controller
 {
@@ -26,9 +27,9 @@ class MarktenController extends Controller
      */
     public function getStandhouderTableJSON(Request $request)
     {
-        $markten = Markt::orderBy('datum', 'desc')->get();
-        return View('users.markten')->with('markten', $markten);
-        // $json = Standhouder::getStandhoudersForMarkt($request->input("markt_id"));
-        // return $json . "<br> test";
+        // $markten = Markt::orderBy('datum', 'desc')->get();
+        // return View('users.markten')->with('markten', $markten);
+        $json = Standhouder::getStandhoudersForMarkt($request->input("markt_id"));
+        return $json . "<br> test";
     }
 }
